77adfd62f2d46f9d15ffcb7d6d26de60ebf27c35
mrozk <mrozk2012@gmail.com>
2016-09-26 14:53:46 +0300

    Init

---
+++ config/db-example.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=xxx',
+    'username' => 'xxxx',
+    'password' => 'xxxxx',
+    'charset' => 'utf8',
+];
